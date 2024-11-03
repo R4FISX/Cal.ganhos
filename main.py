b9f4142bfcda1201a5086433d538d182c5d50f34
R4FISX <rafaelrodrgues002@gmail.com>
2024-11-03 18:31:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 
-# Funções de cálculo
 def calcular_cash():
     try:
         vendas = float(entry_vendas.get())
@@ -24,7 +23,6 @@ def calcular_comissao():
     except ValueError:
         messagebox.showerror("Erro", "Por favor, insira valores numéricos válidos para vendas e percentual de comissão.")
 
-# Função principal para inicializar a interface
 def iniciar_interface():
     global janela
     janela = tk.Tk()
@@ -32,16 +30,13 @@ def iniciar_interface():
     janela.geometry("500x500")
     janela.configure(bg="#0047ab")  # Fundo azul
 
-    # Título centralizado
     label_titulo = tk.Label(janela, text="Calculadora de Ganhos", font=("Arial", 24, "bold"), bg="#0047ab", fg="white")
     label_titulo.pack(pady=20)
 
-    # Campo de entrada para vendas
     global entry_vendas
     entry_vendas = tk.Entry(janela, font=("Arial", 12), width=30)
     entry_vendas.pack(pady=10)
 
-    # Label e campo de entrada manual para percentual de comissão
     global entry_percentual
     label_percentual = tk.Label(janela, text="Digite a % de Comissão:", font=("Arial", 10), bg="#0047ab", fg="white")
     label_percentual.pack(pady=5)
@@ -49,7 +44,6 @@ def iniciar_interface():
     entry_percentual.pack(pady=5)
     entry_percentual.insert(0, "5")  # Valor padrão de 5%
 
-    # Frame para botões de cálculo
     frame_botoes = tk.Frame(janela, bg="#0047ab")
     frame_botoes.pack(pady=20)
 
@@ -70,19 +64,18 @@ def iniciar_interface():
     label_resultado_comissao.pack(pady=5)
 
 
-     # Label para exibir o valor total em Cash
+     # exibir o valor total em Cash
     global label_resultado_cash
     label_resultado_cash = tk.Label(janela, text="Valor total em Cash: R$ 0.00", font=("Arial", 12), bg="#0047ab", fg="white", wraplength=400)
     label_resultado_cash.pack(pady=5)
 
     janela.mainloop()
 
-# Classe para reiniciar o aplicativo quando um arquivo for modificado
 class FileChangeHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.src_path == __file__:
             print("Arquivo modificado, reiniciando...")
-            janela.destroy()  # Fecha a janela atual
+            janela.destroy() 
             os.execv(sys.executable, ['python'] + sys.argv)  # Reinicia o aplicativo
 
 # Função para iniciar o monitoramento de arquivos
@@ -98,9 +91,7 @@ def iniciar_monitoramento():
         observer.stop()
     observer.join()
 
-# Execução da interface e do monitoramento
 if __name__ == "__main__":
-    # Iniciar o monitoramento em uma thread separada
     from threading import Thread
     monitor_thread = Thread(target=iniciar_monitoramento)
     monitor_thread.daemon = True
